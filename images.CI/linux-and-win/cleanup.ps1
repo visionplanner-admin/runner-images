c876de2afd22d95d41164602e2491d907f58ef04
Mels de Zeeuw <mels.dezeeuw@visma.com>
2025-03-27 22:41:34 +0100

    Refactor cleanup task in image generation pipeline to use PowerShell@2 and add parameters for Azure authentication

--- images.CI/linux-and-win/cleanup.ps1
+++ images.CI/linux-and-win/cleanup.ps1
@@ -1,6 +1,13 @@
 param(
-    [Parameter (Mandatory=$true)] [string] $TempResourceGroupName
+    [Parameter (Mandatory=$true)] [string] $TempResourceGroupName,
+    [Parameter (Mandatory=$true)] [string] $ClientId,
+    [Parameter (Mandatory=$true)] [string] $ClientSecret,
+    [Parameter (Mandatory=$true)] [string] $tenantId,
+    [Parameter (Mandatory=$true)] [string] $subscriptionId
 )
+
+az login --service-principal -u $ClientId -p $ClientSecret --tenant $tenantId
+az account set --subscription $subscriptionId
 
 $groupExist = az group exists --name $TempResourceGroupName
 if ($groupExist -eq "true") {
